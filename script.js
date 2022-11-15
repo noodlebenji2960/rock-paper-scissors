d5a36631706fcd54c3260a7a9a9005997e5e5e87
BJC <noodlebenji2960@gmail.com>
2022-11-15 20:25:36 +0100

    adding spock lizard

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-let choices = ["rock", "paper", "scissors"]
+let choices = ["rock", "paper", "scissors", "lizard", "spock"];
+
+function reset() {
+  userScore = parseInt(0);
+  computerScore = parseInt(0);
+}
 
 function getComputerChoice() {
-  return choices[Math.floor(Math.random() * Math.floor(choices.length))]
+  return choices[Math.floor(Math.random() * Math.floor(choices.length))];
 }
 
 let computerSelection = getComputerChoice();
@@ -9,39 +14,49 @@ let userScore = parseInt(0);
 let computerScore = parseInt(0);
 
 function playRound(playerSelection, computerSelection) {
-  if ((playerSelection == 'rock' && computerSelection === 'scissors') || (playerSelection == 'paper' && computerSelection === 'rock') || (playerSelection == 'scissors' && computerSelection === 'paper')) {
+  if ( (playerSelection == 'rock' && (computerSelection === 'scissors' || computerSelection === 'lizard'))
+    || (playerSelection == 'paper' && (computerSelection === 'rock' || computerSelection == 'spock'))
+    || (playerSelection == 'scissors' && (computerSelection === 'paper' || computerSelection === 'lizard'))
+    || (playerSelection == 'spock' && (computerSelection === 'scissors' || computerSelection === 'rock'))
+    || (playerSelection == 'lizard' && (computerSelection === 'paper' || computerSelection === 'spock'))) {
       userScore++;
       return ("Yay! You won the round! " + playerSelection + " beats " + computerSelection);
   }
-  else if ((playerSelection == 'rock' && computerSelection === 'paper') || (playerSelection == 'paper' && computerSelection === 'scissors') || (playerSelection == 'scissors' && computerSelection === 'rock')) {
-      computerScore++;
-      return ("Sorry, you lost the round. " + computerSelection + " beats " + playerSelection);
+  else if (computerSelection === playerSelection){
+      return ("oh no! Looks like a draw!");
   }
   else {
-      return ("oh no! Looks like a draw!");
+      computerScore++;
+      return ("Sorry, you lost the round. " + computerSelection + " beats " + playerSelection);
   }
 }
 
 function game() {
   for (let i = 1; i <6; i++) {
-    let promptText = prompt("Choose either Rock, Paper or Scissors");
+    let promptText = prompt("Choose either Rock, Paper, Scissors, Lizard or Spock");
     let playerSelection = promptText.toLowerCase();
     let computerSelection = getComputerChoice();
-    console.log('\n', "---Round " + i + "---")
-    console.log(playRound(playerSelection, computerSelection))
-    console.log("You chose: " + playerSelection)
-    console.log("Computer chose: " + computerSelection)
+    console.log('\n', "---Round " + i + "---");
+    console.log(playRound(playerSelection, computerSelection));
+    console.log("You chose: " + playerSelection);
+    console.log("Computer chose: " + computerSelection);
     console.log("your score : " + userScore);
     console.log("Computer's score : " + computerScore);
 
     if (i == 5) {
       if (userScore > computerScore){
-        console.log('\n', ("-----G R E A T  W O R K !-----"))
-        console.log(("You won " + userScore + " out of 5 rounds."),'\n',("Computer won " + computerScore + " out of 5 rounds."))
+        console.log('\n', ("-----G R E A T  W O R K !-----"));
+        console.log(("You won " + userScore + " out of 5 rounds."),'\n',("Computer won " + computerScore + " out of 5 rounds."));
       }
       else {
-        console.log('\n', ("-----B A D  L U C K !-----"))
-        console.log(("You won " + userScore + " out of 5 rounds."),'\n',("Computer won " + computerScore + " out of 5 rounds."))
+        if(userScore = computerScore) {
+          console.log('\n', ("-----ITS A DRAW!-----"));
+          console.log(("You won " + userScore + " out of 5 rounds."),'\n',("Computer won " + computerScore + " out of 5 rounds."));
+        }
+        else{
+          console.log('\n', ("-----YOU LOST!-----"));
+          console.log(("You won " + userScore + " out of 5 rounds."),'\n',("Computer won " + computerScore + " out of 5 rounds."));
+        }
       }
     }
   }
@@ -49,7 +64,7 @@ function game() {
 
 let btn = document.getElementById("startgamebutton");
 btn.addEventListener('click', event => {
-  game();
+  game() && reset();
 });
 
-
+//TO DO
