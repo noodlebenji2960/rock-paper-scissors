c20ec2fd76caa33a147633adbbad34fe20d0a4c0
BJC <noodlebenji2960@gmail.com>
2022-11-16 02:02:08 +0100

    completed UI, as per assignment in TOP

--- script.js
+++ script.js
@@ -1,69 +1,84 @@
 let choices = ["rock", "paper", "scissors", "lizard", "spock"];
-
-function resetAndStart() {
-  userScore = 0;
-  computerScore = 0;
-  game();
-}
+let userScore = parseInt(0)
+let computerScore = parseInt(0)
 
 function getComputerChoice() {
   return choices[Math.floor(Math.random() * Math.floor(choices.length))];
 }
 
-let computerSelection = getComputerChoice();
-let userScore = parseInt(0);
-let computerScore = parseInt(0);
-
-function playRound(playerSelection, computerSelection) {
-  if ( (playerSelection == 'rock' && (computerSelection === 'scissors' || computerSelection === 'lizard'))
-    || (playerSelection == 'paper' && (computerSelection === 'rock' || computerSelection == 'spock'))
-    || (playerSelection == 'scissors' && (computerSelection === 'paper' || computerSelection === 'lizard'))
-    || (playerSelection == 'spock' && (computerSelection === 'scissors' || computerSelection === 'rock'))
-    || (playerSelection == 'lizard' && (computerSelection === 'paper' || computerSelection === 'spock'))) {
-      userScore++;
-      return ("Yay! You won the round! " + playerSelection + " beats " + computerSelection);
+function playRound(playerSelection) {
+  let computerSelection = getComputerChoice();
+  if ( (playerSelection === 'rock' && (computerSelection === 'scissors' || computerSelection === 'lizard'))
+    || (playerSelection === 'paper' && (computerSelection === 'rock' || computerSelection == 'spock'))
+    || (playerSelection === 'scissors' && (computerSelection === 'paper' || computerSelection === 'lizard'))
+    || (playerSelection === 'spock' && (computerSelection === 'scissors' || computerSelection === 'rock'))
+    || (playerSelection === 'lizard' && (computerSelection === 'paper' || computerSelection === 'spock'))) {
+      userScore++
+      document.getElementById("results").textContent = "Yay! You won the round! " + playerSelection + " beats " + computerSelection;
+      updateScore()
   }
   else if (computerSelection === playerSelection){
-      return ("oh no! Looks like a draw!");
+    document.getElementById("results").textContent = "oh no! Looks like a draw!";
+    updateScore()
   }
   else {
-      computerScore++;
-      return ("Sorry, you lost the round. " + computerSelection + " beats " + playerSelection);
+    computerScore++
+    document.getElementById("results").textContent = "Sorry, you lost the round. " + computerSelection + " beats " + playerSelection;
+    updateScore()
   }
 }
 
-function game() {
-  for (let i = 1; i <6; i++) {
-    let promptText = prompt("Choose either Rock, Paper, Scissors, Lizard or Spock");
-    let playerSelection = promptText.toLowerCase();
-    let computerSelection = getComputerChoice();
-    console.log('\n', "---Round " + i + "---");
-    console.log(playRound(playerSelection, computerSelection));
-    console.log("You chose: " + playerSelection);
-    console.log("Computer chose: " + computerSelection);
-    console.log("your score : " + userScore);
-    console.log("Computer's score : " + computerScore);
+function hideOnboarding() {
+  document.getElementById("videoContainer").innerHTML = "";
+}
 
-    if (i == 5) {
-      if (userScore > computerScore){
-        console.log('\n', ("-----G R E A T  W O R K !-----"));
-        console.log(("You won " + userScore + " out of 5 rounds."),'\n',("Computer won " + computerScore + " out of 5 rounds."));
-      }
-      else {
-        if(userScore = computerScore) {
-          console.log('\n', ("-----ITS A DRAW!-----"));
-          console.log(("You won " + userScore + " out of 5 rounds."),'\n',("Computer won " + computerScore + " out of 5 rounds."));
-        }
-        else{
-          console.log('\n', ("-----YOU LOST!-----"));
-          console.log(("You won " + userScore + " out of 5 rounds."),'\n',("Computer won " + computerScore + " out of 5 rounds."));
-        }
-      }
+function updateScore() {
+  document.getElementById("score").textContent ="YOUR SCORE: " + userScore + " COMPUTER SCORE: " + computerScore;
+
+  if (userScore == 5 || computerScore == 5)
+    if(userScore >computerScore) {
+      document.getElementById("results").textContent = "Congratulations You have Won!!";
+      document.getElementById("optionsButtons").innerHTML = "";
     }
-  }
+    else if (userScore == computerScore) {
+      document.getElementById("results").textContent = "It's a draw!";
+      document.getElementById("optionsButtons").innerHTML = "";
+    }
+    else {
+      document.getElementById("results").textContent = "GAME OVER!";
+      document.getElementById("optionsButtons").innerHTML = "";
+    }
 }
 
-let btn = document.getElementById("startgamebutton");
-btn.addEventListener('click', event => {
-  (resetAndStart());
+let hideOnboardingBtn = document.getElementById("hideOnboardingButton");
+hideOnboardingBtn.addEventListener('click', event => {
+  (hideOnboarding());
 });
+
+let rockBtn = document.getElementById("rockButton");
+rockBtn.addEventListener('click', event => {
+  (playRound("rock"));
+});
+
+let paperBtn = document.getElementById("paperButton");
+paperBtn.addEventListener('click', event => {
+  (playRound("paper"));
+});
+
+let scissorsBtn = document.getElementById("scissorsButton");
+scissorsBtn.addEventListener('click', event => {
+  (playRound("scissors"));
+});
+
+let lizardBtn = document.getElementById("lizardButton");
+lizardBtn.addEventListener('click', event => {
+  (playRound("lizard"));
+});
+
+let spockBtn = document.getElementById("spockButton");
+spockBtn.addEventListener('click', event => {
+  (playRound("spock"));
+});
+
+//TO DO
+//change this beats that to rock smashes lizard, spock vaporizes rock, etc.
